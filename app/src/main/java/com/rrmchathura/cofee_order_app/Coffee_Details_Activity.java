f74ce3763fe458e696320bd5929995d5c700f1f7
Chathura Sanjaya <49064590+rrmchathura@users.noreply.github.com>
2022-04-14 20:57:01 +0530

    sizechecked done

--- app/src/main/java/com/rrmchathura/cofee_order_app/Coffee_Details_Activity.java
+++ app/src/main/java/com/rrmchathura/cofee_order_app/Coffee_Details_Activity.java
@@ -15,13 +15,16 @@ import com.squareup.picasso.Picasso;
 public class Coffee_Details_Activity extends AppCompatActivity {
 
     ActivityCoffeeDetailsBinding binding;
-    private String coffeeName,coffeePrice,coffeeImage;
+    private String coffeeName, coffeePrice, coffeeImage;
 
     private int Quantity = 1;
     private int finalCost = 0;
     private int cost = 0;
-    private  double SizeCost,SugarCost;
-    private  double finalSizeCost,finalSugarCost;
+    private double smallSizeCost, mediumSizeCost,largeSizeCost;
+    private double finalSizeCost;
+    boolean smallSizeChecked = false;
+    boolean mediumSizeChecked = false;
+    boolean largeSizeChecked = false;
 
 
     @Override
@@ -46,8 +49,8 @@ public class Coffee_Details_Activity extends AppCompatActivity {
 
         binding.coffeeName.setText(coffeeName);
         binding.coffeeNameTwo.setText(coffeeName);
-        binding.priceTv.setText("Rs."+coffeePrice);
-        binding.totalPriceTv.setText("Rs."+coffeePrice);
+        binding.priceTv.setText("Rs." + coffeePrice);
+        binding.totalPriceTv.setText("Rs." + coffeePrice);
 
         cost = Integer.valueOf(coffeePrice);
         finalCost = Integer.parseInt(coffeePrice);
@@ -56,11 +59,43 @@ public class Coffee_Details_Activity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                if (smallSizeChecked == true) {
+
                     finalCost = finalCost + cost;
                     Quantity++;
 
-                    binding.totalPriceTv.setText("Rs."+String.valueOf(SizeCost+finalCost+SugarCost));
+                    binding.totalPriceTv.setText("Rs." + String.valueOf(smallSizeCost + finalCost));
                     binding.quantityTv.setText(String.valueOf(Quantity));
+                    SizePrice(finalCost);
+
+                } else if (mediumSizeChecked == true){
+
+                    finalCost = finalCost + cost;
+                    Quantity++;
+
+                    binding.totalPriceTv.setText("Rs." + String.valueOf(smallSizeCost + finalCost));
+                    binding.quantityTv.setText(String.valueOf(Quantity));
+                    mediumSizePrice(finalCost);
+
+                }
+                else if (largeSizeChecked == true){
+
+                    finalCost = finalCost + cost;
+                    Quantity++;
+
+                    binding.totalPriceTv.setText("Rs." + String.valueOf(smallSizeCost + finalCost));
+                    binding.quantityTv.setText(String.valueOf(Quantity));
+                    largeSizePrice(finalCost);
+
+                }
+                else {
+                    finalCost = finalCost + cost;
+                    Quantity++;
+
+                    binding.totalPriceTv.setText("Rs." + String.valueOf(smallSizeCost + finalCost));
+                    binding.quantityTv.setText(String.valueOf(Quantity));
+
+                }
 
 
             }
@@ -69,13 +104,50 @@ public class Coffee_Details_Activity extends AppCompatActivity {
         binding.btnDecrement.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (Quantity > 1) {
-                    finalCost = finalCost - cost;
-                    Quantity--;
 
-                    binding.totalPriceTv.setText("Rs."+String.valueOf(SizeCost+finalCost+SugarCost));
-                    binding.quantityTv.setText(String.valueOf(Quantity));
+                if (smallSizeChecked == true) {
+
+                    if (Quantity > 1) {
+                        finalCost = finalCost - cost;
+                        Quantity--;
+
+                        binding.totalPriceTv.setText("Rs." + String.valueOf(smallSizeCost + finalCost));
+                        binding.quantityTv.setText(String.valueOf(Quantity));
+                        SizePrice(finalCost);
+                    }
+                } else if (mediumSizeChecked == true){
+
+                    if (Quantity > 1) {
+                        finalCost = finalCost - cost;
+                        Quantity--;
+
+                        binding.totalPriceTv.setText("Rs." + String.valueOf(smallSizeCost + finalCost));
+                        binding.quantityTv.setText(String.valueOf(Quantity));
+                        mediumSizePrice(finalCost);
+                    }
                 }
+                else if (largeSizeChecked == true){
+                    if (Quantity > 1) {
+                        finalCost = finalCost - cost;
+                        Quantity--;
+
+                        binding.totalPriceTv.setText("Rs." + String.valueOf(smallSizeCost + finalCost));
+                        binding.quantityTv.setText(String.valueOf(Quantity));
+                        largeSizePrice(finalCost);
+                    }
+                }
+
+                else {
+                    if (Quantity > 1) {
+                        finalCost = finalCost - cost;
+                        Quantity--;
+
+                        binding.totalPriceTv.setText("Rs." + String.valueOf(smallSizeCost + finalCost));
+                        binding.quantityTv.setText(String.valueOf(Quantity));
+                    }
+                }
+
+
             }
         });
 
@@ -83,9 +155,11 @@ public class Coffee_Details_Activity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                SizeCost = finalCost * 0.1;
-                finalSizeCost = finalCost+SizeCost;
-                binding.totalPriceTv.setText("Rs."+String.valueOf(finalSizeCost));
+                smallSizeChecked = true;
+                mediumSizeChecked = false;
+                largeSizeChecked = false;
+
+                SizePrice(finalCost);
             }
         });
 
@@ -93,9 +167,13 @@ public class Coffee_Details_Activity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                SizeCost = finalCost * 0.2;
-                finalSizeCost = finalCost+SizeCost;
-                binding.totalPriceTv.setText("Rs."+String.valueOf(finalSizeCost));
+                mediumSizeChecked = true;
+                smallSizeChecked = false;
+                largeSizeChecked = false;
+
+                mediumSizePrice(finalCost);
+
+
             }
         });
 
@@ -103,21 +181,23 @@ public class Coffee_Details_Activity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                SizeCost = finalCost * 0.3;
-                finalSizeCost = finalCost+SizeCost;
-                binding.totalPriceTv.setText("Rs."+String.valueOf(finalSizeCost));
+                largeSizeChecked = true;
+                smallSizeChecked = false;
+                mediumSizeChecked = false;
+
+                largeSizePrice(finalCost);
             }
         });
-
-        binding.noSugarRB.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                SugarCost = finalCost * 0.1;
-                finalSugarCost = finalCost+SugarCost;
-                binding.totalPriceTv.setText("Rs."+String.valueOf(finalSugarCost));
-            }
-        });
+//
+//        binding.noSugarRB.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//                SugarCost = finalCost * (-0.1); //-10%
+//                finalSugarCost = finalCost+SugarCost;
+//                binding.totalPriceTv.setText("Rs."+String.valueOf(finalSugarCost));
+//            }
+//        });
 
 
     }
@@ -126,5 +206,23 @@ public class Coffee_Details_Activity extends AppCompatActivity {
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return super.onSupportNavigateUp();
+    }
+
+    public void SizePrice(int finalCost) {
+        smallSizeCost = finalCost * 0.1;
+        finalSizeCost = finalCost + smallSizeCost;
+        binding.totalPriceTv.setText("Rs." + String.valueOf(finalSizeCost));
+    }
+
+    public void mediumSizePrice(int finalCost) {
+        mediumSizeCost = finalCost * 0.2;
+        finalSizeCost = finalCost + mediumSizeCost;
+        binding.totalPriceTv.setText("Rs." + String.valueOf(finalSizeCost));
+    }
+
+    public void largeSizePrice(int finalCost) {
+        largeSizeCost = finalCost * 0.3;
+        finalSizeCost = finalCost+largeSizeCost;
+        binding.totalPriceTv.setText("Rs."+String.valueOf(finalSizeCost));
     }
 }
